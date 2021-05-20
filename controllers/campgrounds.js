447a235865a6d53dcb6f2c928004c0e5436e7ca3
GorillaDev <germansanz93@gmail.com>
2021-05-20 10:56:23 -0300

    -created campgrounds controller -created reviews controller -created users controller -routes grouped -starability feature added

---
+++ controllers/campgrounds.js
@@ -0,0 +1,58 @@
+const Campground = require('../models/Campground')
+
+module.exports.index = async (req, res) => {
+    const campgrounds = await Campground.find({});
+    res.render('campgrounds/index', { campgrounds });
+}
+
+module.exports.renderNewForm = (req, res) => {
+    res.render('campgrounds/new');
+}
+
+module.exports.createCampground = async (req, res, next) => {
+    // if (!req.body.campground) throw new ExpressError('Invalid Campground data', 400);
+    const campground = new Campground(req.body.campground);
+    campground.author = req.user._id;
+    await campground.save();
+    req.flash('success', 'Succesfully made a new campground!')
+    res.redirect(`/campgrounds/${campground._id}`);
+}
+
+module.exports.showCampground = async (req, res) => {
+    const { id } = req.params;
+    const campground = await Campground.findById(id).populate({
+        path: 'reviews', //populate anidado, hace populate a revieews y dentro de 
+        populate: { //reviews hace populate a los author del review
+            path: 'author'
+        }
+    }).populate('author');//luego hace populate a los author del camp
+    if (!campground) {
+        req.flash('error', 'Cannot find that campground!');
+        return res.redirect('/campgrounds');
+    }
+    res.render('campgrounds/show', { campground });
+}
+
+module.exports.renderEditForm = async (req, res) => {
+    const { id } = req.params;
+    const campground = await Campground.findById(id); //esta forma sirve tambien si no quiero hacer undestructurind de params
+    if (!campground) {
+        req.flash('error', 'Cannot find that campground!');
+        return res.redirect('/campgrounds');
+    }
+    res.render('campgrounds/edit', { campground });
+}
+
+module.exports.updateCampground = async (req, res) => {
+    const { id } = req.params;
+    const campground = await Campground.findByIdAndUpdate(id, { ...req.body.campground }); //hizo spread sobre campground en body, porque de la forma que nombramos los parametros quedan agrupados dentro de campground
+    req.flash('success', 'Successfully updated campground');
+    res.redirect(`/campgrounds/${campground._id}`);
+}
+
+module.exports.deleteCampground = async (req, res) => {
+    const { id } = req.params;
+    await Campground.findByIdAndDelete(id);
+    req.flash('success', 'Successfully deleted campground');
+    res.redirect('/campgrounds');
+}
