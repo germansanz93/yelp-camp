028f5a6722b43e0d0e5c9ebe3e7bb5241d53877e
GorillaDev <germansanz93@gmail.com>
2021-05-21 12:51:54 -0300

    image upload cycle completed

--- models/campground.js
+++ models/campground.js
@@ -4,7 +4,12 @@ const Schema = mongoose.Schema; //atajo para no tener que escribir mongoose.Sche
 
 const CampgroundSchema = new Schema({
     title: String,
-    image: String,
+    images: [
+        {
+            url: String,
+            filename: String
+        }
+    ],
     price: Number,
     description: String,
     location: String,
--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -5,15 +5,12 @@ const router = express.Router();
 const { isLoggedIn, validateCampground, isAuthor } = require('../middleware');
 const campgrounds = require('../controllers/campgrounds');
 const multer = require('multer');
-const upload = multer({ dest: 'uploads/' });
+const { storage } = require('../cloudinary');
+const upload = multer({ storage });
 
 router.route('/')
     .get(catchAsync(campgrounds.index))
-    .post(upload.array('image'), (req, res) => {
-        console.log(req.body, req.files);
-        res.send('hi');
-    })
-// .post(isLoggedIn, validateCampground, catchAsync(campgrounds.createCampground));
+    .post(isLoggedIn, upload.array('image'), validateCampground, catchAsync(campgrounds.createCampground));
 
 router.get('/new', isLoggedIn, campgrounds.renderNewForm);
 
